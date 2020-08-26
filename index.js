b5ca73470098037197a6878eb9dc78f88b7547a3
Xoa Cox <joakkoa@gmail.com>
2020-08-26 14:27:03 -0300

    added sound on keydown

--- index.js
+++ index.js
@@ -2,54 +2,63 @@
 //https://developer.mozilla.org/es/docs/Web/API/EventTarget/addEventListener
 //de esta forma le damos un metodo a click que en este caso seria handleClick();
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
-
+//detectar click
 for(var i=0; i<numberOfDrumButtons;i++){
     document.querySelectorAll(".set .drum")[i].addEventListener("click",function(){
         var buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML){
-            case "w":
-                var tom1 = new Audio('sounds/tom-1.mp3');
-                tom1.play();
-                break;
-                
-            case "a":
-                var tom2 = new Audio('sounds/tom-2.mp3');
-                tom2.play();
-                break;
-            
-            case "s":
-                var tom3 = new Audio('sounds/tom-3.mp3');
-                tom3.play();
-                break;
-
-            case "d":
-                var tom4 = new Audio('sounds/tom-4.mp3');
-                tom4.play();
-                break;
-
-            case "j":
-                var snare = new Audio('sounds/snare.mp3');
-                snare.play();
-                break;
-            
-            case "k":
-                var crash = new Audio('sounds/crash.mp3');
-                crash.play();
-                break;
-
-            case "l":
-                var kick = new Audio('sounds/kick-bass.mp3');
-                kick.play();
-                break;
-                
-            default: 
-                console.log();
-
-
-        }
+        makeSound(buttonInnerHTML);
         //que hacer cuando se detecta un click
     });
 
+}
+//detectar teclado
+document.addEventListener("keydown", function(event){
+    makeSound(event.key)
+});
+
+//revisar la tecla hacer ruido
+function makeSound(key){
+    switch (key){
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+            
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        
+        case "k":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "l":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+            
+        default: 
+            console.log();
+
+
+    }
 }
 
 //tambien podria haberse hecho de esta forma mas compacta:
